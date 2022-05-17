44d82446782b11d02c2a63a8208035ea977aec55
SeonSangWon <sangwon7482@naver.com>
2022-05-17 22:39:28 +0900

    날씨 코드 Parser 생성

--- codi-crawling/server/route/parser.js
+++ codi-crawling/server/route/parser.js
@@ -4,9 +4,13 @@ import { app } from '../index';
 import { logger } from '../../logger';
 import { getWeatherGPSCode } from '../service/parser';
 
-const handler = async () => {
-    const result = await getWeatherGPSCode();
-    console.info(result);
+const handler = async (req, res, next) => {
+    try {
+      return  await getWeatherGPSCode();
+    } catch (err) {
+      logger.error(`handler`, err);
+      next(err);
+    }
 };
 
 export default () => {
